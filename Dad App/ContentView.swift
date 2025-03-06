d77993666b08da83b258e82ed881a450a5bb3453
Ash Beech <ash@bullish.design>
2025-03-06 11:23:40 +0000

    Initial Commit

---
+++ Dad App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dad App
+//
+//  Created by Ashley Davison on 06/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dad AppTests/Dad_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_AppTests.swift
+//  Dad AppTests
+//
+//  Created by Ashley Davison on 06/03/2025.
+//
+
+import Testing
+@testable import Dad_App
+
+struct Dad_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
